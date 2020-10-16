d32d2557f8522a84b6d4ae2755d9b7447fe430e2
Fofan228 <71459985+Fofan228@users.noreply.github.com>
2020-10-16 17:59:33 +0500

    Add files via upload

---
+++ Factrorial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DZ
+{
+    class Program
+    {
+         public static void WriteUPandDownWalls(int maxLength, char beginWall, char indentWall, char endWall)
+        {
+            Console.Write(beginWall);
+            for (int i = 0; i < maxLength; i++)
+                Console.Write(indentWall);
+            Console.WriteLine(endWall);
+        }
+
+        public static void WriteMidlleWalls(int maxLength, char beginWall, char indentWall, char endWall, string result)
+        {
+            Console.Write(beginWall);
+            Console.Write(indentWall);
+            Console.Write(result);
+            
+            int imitation = maxLength - result.Length - 1;
+            for (int i = 0; i < imitation; i++)
+                Console.Write(indentWall);
+
+            Console.WriteLine(endWall);
+        }
+
+
+        static int GetMaxLength(int result)
+        {
+            string max = result.ToString();
+            return max.Length;
+        }
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+
+            int input = int.Parse(Console.ReadLine());
+            int result = Factorial(input);
+            int maxLength = GetMaxLength(result) + 2;
+            string str = Convert.ToString(result);
+
+            WriteUPandDownWalls(maxLength, '╔', '═', '╗');
+            WriteUPandDownWalls(maxLength, '║', ' ', '║');
+         
+                WriteMidlleWalls(maxLength, '║', ' ', '║', str);
+
+            WriteUPandDownWalls(maxLength, '║', ' ', '║');
+            WriteUPandDownWalls(maxLength, '╚', '═', '╝');
+
+        }
+    }
+}
